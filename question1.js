822b7c6e82988e3f14f7df367b41b1e586193986
Afrah79 <afrahmubarakalazmi@gmail.com>
2021-01-05 22:45:30 +0300

    done

--- question1.js
+++ question1.js
@@ -1,1 +1,28 @@
 // قم بحل السؤال الأول هنا
+let pets = ["dog", "cat","rabbit","hamster","parrot"];
+
+pets.pop();
+
+pets.push("fish");
+
+pets [2]= "kitten";
+
+
+
+
+let cat ={
+   Name: "loki",
+   Age: "3months",
+   Breed: "Scotish fold",
+};
+
+cat.emoji = "😺";
+
+console.log(cat)
+console.log(cat["Age"]);
+
+
+
+
+
+
